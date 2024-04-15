f683b8e0322aea15909b0b90db81443147067d08
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-15 02:14:31 +0200

    Initial Commit

---
+++ AppleRandom/AppleRandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleRandomApp.swift
+//  AppleRandom
+//
+//  Created by  Ixart on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleRandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
