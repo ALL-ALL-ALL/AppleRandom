8aba237fe40b26aeab8fe18ed2f00140de41af7b
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-16 01:54:20 +0200

    ✅

--- AppleRandom/ContentView.swift
+++ AppleRandom/ContentView.swift
@@ -1,43 +1,56 @@
-//
-//  ContentView.swift
-//  AppleRandom
-//
-//  Created by  Ixart on 15/04/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var appleLogoPosition: CGPoint = .zero
+    
+    @State private var logoColorIndex = 1
+
+    private var logoColors: [Color] = [.red, .green, .blue, .orange, .yellow, .red, .pink, .gray, .brown, .mint, .cyan]
+
+
     var body: some View {
         ZStack {
             Color(.black)
 
             VStack {
-                
                 Image(systemName: "applelogo")
-                    .foregroundColor(.blue)
+                    .foregroundColor(logoColors[logoColorIndex]) // change couleur via tableau
                     .font(.system(size: 110))
-                
-            } // fin vstack
-        } // fin zstack
+                    .position(appleLogoPosition)
+                    .gesture(
+                        TapGesture()
+                            .onEnded { _ in
+                                appleLogoPosition = randomPosition()
+                                cycleLogoColor()
+                                
+                            } // fin in
+                    )
+            } // fin VStack
+        } // fin ZStack
         .ignoresSafeArea()
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     } // fin body
-} // fin struct
+
+    // Fonction pour générer une nouvelle position aléatoire dans la vue
+    private func randomPosition() -> CGPoint {
+        let maxX = UIScreen.main.bounds.width
+        let maxY = UIScreen.main.bounds.height
+
+        let randomX = CGFloat.random(in: 1..<maxX)
+        let randomY = CGFloat.random(in: 1..<maxY)
+        return CGPoint(x: randomX, y: randomY)
+    } // func random
+    
+    private func cycleLogoColor() {
+        logoColorIndex = (logoColorIndex + 1) % logoColors.count
+
+       } // fin func
+    
+    
+    
+    
+} // struct
+
+
 
 #Preview {
     ContentView()
